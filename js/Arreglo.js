efa62f18edf3296fc70ffc423b6bc25a04b9fe75
javier <javier.puentes01@gmail.com>
2022-10-13 17:17:03 -0500

    ejecutar el metodo split() dentro del if

--- js/Arreglo.js
+++ js/Arreglo.js
@@ -6,15 +6,15 @@ class Arreglo {
     localStorage.setItem("arreglo_palabra", this.palabras);
   }
   optener_locaStorage() {
-    let palaba_storage = localStorage.getItem("arreglo_palabra").split(",");
+    let palaba_storage = localStorage.getItem("arreglo_palabra");
     if(palaba_storage != ""){
-      this.palabras = palaba_storage;
+      this.palabras = palaba_storage.split(",");
     }
   }
   new_palabra(palabra) {
-    let palaba_storage = localStorage.getItem("arreglo_palabra").split(",");
+    let palaba_storage = localStorage.getItem("arreglo_palabra");
     if(palaba_storage != ""){
-      this.palabras = palaba_storage;
+      this.palabras = palaba_storage.split(",");
     }
     this.palabras.push(palabra);
     this.enviar_locaStorage();
